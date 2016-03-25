c32f098fa0bff325a3aa2b550075dce81953bf69
Jacob John <jacob.john@fsconlt-JacobJ.local>
2016-03-25 15:24:39 -0400

    Further abstracting character input module

--- core.js
+++ core.js
@@ -5,39 +5,59 @@
 var animatedCharacterInput = (function() {
   'use strict';
 
-  var animatedCharacterInput = {
-    init: function(animation) {
+  // Character Input Constructor
 
-      const $input = $('#textInput');
-      var storedText = "";
+  function characterInput() {
+
+    this.template = '<div class="animatedInput"><input id="textInput" type="text" maxlength="24"></input><div class="start"></div></div>'
+    this.animation = "slideInUp"
+
+  }
+
+  // Render Template and Element to View
+  characterInput.prototype.render = function() {
+
+    var template = this.template;
+
+    var characterInputPrototype = Object.create(HTMLElement.prototype);
+    characterInputPrototype.createdCallback = function() {
+
+      this.innerHTML = template;
+
+    };
+
+    characterInputPrototype.foo = function() {
+      console.log('foo() called');
+    };
+
+    var XFoo = document.registerElement('character-input', {
+      prototype: characterInputPrototype
+    });
+
+  }
+
+  characterInput.prototype.observe = function() {
+
+      const $input        = $('#textInput');
+      const animationType = this.animation;
       
-      var keyDowns = Rx.Observable.fromEvent($input, 'keydown');
-      var inputChange = Rx.Observable.fromEvent($('#textInput'), 'select');
+      var keyDowns    = Rx.Observable.fromEvent($input, 'keydown');
+      var inputChange = Rx.Observable.fromEvent($input, 'select');
 
       var actions = Rx.Observable
-          .merge(keyDowns)
-          .filter((function() {
-              var keysPressed = {};
-              return function(e) {
+          .merge(inputChange, keyDowns)
+          .filter( function(x, idx, obs) {
+            // console.log(idx);
+            return idx;
 
-                  // filter logic here maybe
-
-                  return e;
-                
-              };
-          })());
+          });
 
       keyDowns.subscribe(function(e) {
-          // console.log(e);
-          // console.log(e.type, e.key || e.which, e.keyIdentifier);
+
           var character = ""+e.keyIdentifier+"";
           var val = String.fromCharCode(parseInt(event.keyIdentifier.substr(2), 16)).toLowerCase();
           if(e.keyCode === 32) { val = "&nbsp"; }
           if(e.keyCode === 8) { 
-
-
-            // $(".animatedInput div:last-child").remove();
-            
 
             setTimeout( function() {
 
@@ -47,31 +67,62 @@ var animatedCharacterInput = (function() {
               if(e.target.value[i] === " ") { val = "&nbsp;" }
               $(".animatedInput").append("<div class='animated'>"+val+"</div>");
             }
-            }, 10);
+            }, 1);
 
           }
           else {
             
             if($(".animatedInput").find("div:not(.start)").length <= 24) {      
-              $(".animatedInput div").eq(e.target.selectionStart).after("<div class='animated "+ animation +"'>"+val+"</div>");
+              $(".animatedInput div").eq(e.target.selectionStart).after("<div class='animated "+ animationType +"'>"+val+"</div>");
             }
 
           }
 
       });
 
-      inputChange.subscribe(function(e) {
-        console.log(e);
-        // setTimeout( function() {
+      var observableArray = [];
+      var i = 0;
 
-        //       $(".animatedInput").find("div:not(.start)").remove();
-        //     for (var i = 0; i < e.target.value.length; i++) {
-        //       var val = e.target.value[i];
-        //       if(e.target.value[i] === " ") { val = "&nbsp;" }
-        //       $(".animatedInput").append("<div class='animated'>"+val+"</div>");
-        //     }
-        //     }, 10);
-      });
+      actions.subscribe(
+        function(e) {
+
+          observableArray.push(e);
+          i++;
+          
+          console.log(e);
+
+          if( i > 1 ) { 
+            if(observableArray[i-2].type === "select") {
+
+              setTimeout( function() {
+
+                $(".animatedInput").find("div:not(.start)").remove();
+              for (var i = 0; i < e.target.value.length; i++) {
+                var val = e.target.value[i];
+                if(e.target.value[i] === " ") { val = "&nbsp;" }
+                $(".animatedInput").append("<div class='animated'>"+val+"</div>");
+              }
+              }, 1);
+
+            }
+          }
+
+        }
+      );
+
+  }
+
+  var animatedCharacterInput = {
+    init: function(animation) {
+
+      var input = new characterInput();
+          input.render()
+          input.observe()
+    
+    },
+    changeAnimation: function() {
+      
+      
 
     }
   };
@@ -79,22 +130,6 @@ var animatedCharacterInput = (function() {
   return animatedCharacterInput;
 })();
 
-
-var characterInputPrototype = Object.create(HTMLElement.prototype);
-characterInputPrototype.createdCallback = function() {
-  // this.textContent = "I'm an x-foo!";
-  this.innerHTML = '<div class="animatedInput"><input id="textInput" type="text" maxlength="24"></input><div class="start"></div></div>'
-
-  animatedCharacterInput.init(this.attributes[0].value);
-
-};
-
-characterInputPrototype.foo = function() {
-  console.log('foo() called');
-};
-
-var XFoo = document.registerElement('character-input', {
-  prototype: characterInputPrototype
-});
+animatedCharacterInput.init();
 
 /*=====  End of Animated Input Plugin  ======*/
